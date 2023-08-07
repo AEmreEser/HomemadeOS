fbf4884b89d99fc9b43993fe97190ed76bc80d55
Emre Eser <emre.eser@sabanciuniv.edu>
2023-08-07 20:04:09 +0300

    text & data sections + compile shell script

--- src/main.asm
+++ src/main.asm
@@ -1,15 +1,15 @@
 [org 0x7C00]
 bits 16
 
-
+.TEXT
 main:
 
-    mov bp, 0x8000 ; stack pase
-    mov sp, bp
+    mov bp, 0x8000 ; stack base
+    mov sp, bp ; stack empty
 
     mov ah, 0x0e ; scrollign teletype - print chr and mov cursor interrupt spec for interrupt number 10
 
-    mov bx, os_str
+    mov bx, os_str ; prints os name and version
     call print_str
     mov bx, version_str
     call print_str
@@ -25,7 +25,7 @@ print_chr_scroll: ; bx must contain address of char, al must contain char ascii 
     int 0x10
     ret
 
-print_str:
+print_str: ; bx must contain address of string, the string should be zero terminated
     mov al, [bx]
     cmp al, 0
     je return
@@ -34,10 +34,13 @@ print_str:
 return:
     ret
 
-os_str:
-    db "HomemadeOS",0
-version_str:
-    db " 0.1",0
+
+
+.DATA
+os_str db "HomemadeOS",0
+version_str db " 0.1",0
+
+
 
 times 510-($-$$) db 0
 dw 0AA55h
