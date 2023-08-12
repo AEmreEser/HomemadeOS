f73d104dce09828f3af5009599a7abd7a3f8531d
AEmreEser <emre.eser@sabanciuniv.edu>
2023-08-13 00:57:11 +0300

    reading disk sectors adjacent to the boot sector

--- src/main.asm
+++ src/main.asm
@@ -2,7 +2,7 @@
 bits 16
 
 .TEXT
-main:
+main: ; BOOTSECTOR
 
     mov bp, 0x8000 ; stack base
     mov sp, bp ; stack empty
@@ -13,11 +13,36 @@ main:
     call print_str
     mov bx, version_str
     call print_str
-    
-    mov dx, 0x23af
+    mov bx, endline
+    call print_str
+
+    mov [BOOT_DRIVE], dl 
+    ; al : # of disk sectors to be read
+    ; ch : cylinder # 0 for default hdd boot
+    ; cl : sector # 1 for default hdd boot: can at the lowest be 1
+    ; dh : head # 0 for default hdd boot
+    ; dl : disk drive # 0x80 or 0x81 ---> will be set by the BIOS before moving exec to boot sector (x86 does this, it is not a standard etc.)
+    ; stores read data at location: 0x90000
+    ; bx : where to write read data, segment used: es
+    mov al, 0x05
+    mov ch, 0x00 ; cylinder 0
+    mov cl, 0x02 ; read from after the boot sector - each sector is 512 bytes
+    mov dh, 0x00 ; head 0
+    mov dl, [BOOT_DRIVE]
+    mov bx, 0x0000
+    mov es, bx
+    mov bx, 0x9000
+    call read_disk_sectors
+    mov cx, 0
+    mov bx, 0x9000
+
+word_monitoring_loop:
+    mov dx, [bx]  
     call print_hex
-    mov dx, 0x2345
-    call print_hex
+    inc cx
+    add bx, 2 ; address of next word - 2 bytes
+    cmp cx, 0x101 ; 257 words -> enough to move into the third sector
+    jne word_monitoring_loop
 
     hlt
 
@@ -34,22 +59,22 @@ print_chr_scroll: ; bx must contain address of char, al must contain char ascii 
 
 print_str: ; bx must contain address of string, the string should be zero terminated
     pusha
-print_str_loop:
+    print_str_loop:
     mov al, [bx]
     cmp al, 0
     je return_print_str
     call print_chr_scroll
     jmp print_str_loop
-return_print_str:
+    return_print_str:
     popa
     ret
+; #### PRINT STR END ####
 
-
-print_hex:
+print_hex: ; dx must contain hex number (16 bits)
     pusha
     mov cx, 4 ; counter
 
-hex_loop:
+    hex_loop:
     mov ax, dx ; copy hex
     and ax, 0xf ; last digit in ax
     shr dx, 4 ; move to next digit
@@ -86,6 +111,30 @@ hex_loop:
 
     popa
     ret
+; #### PRINT HEX END ####
+
+read_disk_sectors:
+    ; al : # of disk sectors to be read
+    ; ch : cylinder # 0 for default hdd boot
+    ; cl : sector # 1 for default hdd boot
+    ; dh : head # 0 for default hdd boot
+    ; dl : disk drive # 0x80 or 0x81 ---> will be set by the BIOS before moving exec to boot sector (x86 does this, it is not a standard etc.)
+    ; stores read data at location: 0x90000
+    pusha
+    mov ah, 0x02
+    int 0x13
+check_read_fail:
+    jc disk_read_fail
+    popa
+    ret
+disk_read_fail:
+    mov bx, fail_disk_str
+    call print_str
+    mov dl, ah
+    mov dh, 0x00
+    call print_hex
+    popa
+    ret
 
 
 ; global variables
@@ -98,7 +147,18 @@ os_str:
     db "HomemadeOS",0
 version_str: 
     db " 0.1",0
+endline:
+    dw 0x0a0d,0
+fail_disk_str:
+    db "Disk Fail Code: ", 0
 
+
+BOOT_DRIVE:
+    db 0
+; SPECIFY THE # OF BYTES HERE!!!!
 
 times 510-($-$$) db 0
 dw 0AA55h
+; END OF BOOT SECTOR - anything after this line won't initally be loaded but will be on the disk
+times 256 dw 0xda12 ; 512 bytes of da12
+times 256 dw 0xefce ; 512 bytes of 0xefce
