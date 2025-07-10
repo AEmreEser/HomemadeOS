89e737efad6cb08eaa502e7971526c82b710076f
AEmreEser <emre.eser@sabanciuniv.edu>
2025-07-10 17:42:26 +0300

    important bug fix - bit masks in idt low/high offsets

--- src/idt.c
+++ src/idt.c
@@ -6,8 +6,8 @@ IDT_entry_t idt[NUM_IDT];
 void init_idt_entry(IDT_entry_t * const idt, uint8_t order, uint32_t offset_entry, uint16_t selector_entry, uint8_t attr){
     // 5 primitive moves, implemented this way cause C does not have RVO
     idt[order].attr = attr;
-    idt[order].offset_entry_high = ((offset_entry & 0xff00) >> 16);
-    idt[order].offset_entry_low = offset_entry & 0xff;
+    idt[order].offset_entry_high = ((offset_entry & 0xffff) >> 16);
+    idt[order].offset_entry_low = offset_entry & 0xffff;
     idt[order].selector_entry = selector_entry;
     idt[order].RESERVED = 0;
 }
